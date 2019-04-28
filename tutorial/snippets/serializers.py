9e0ec06326376a1bf8b5eb193f159242d3558eab
Penelope Chua <pche99@gmail.com>
2019-04-29 01:12:11 +0800

    Add log-in with user

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,9 +3,12 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('owner','id', 'title', 'code', 'linenos', 'language', 'style')
+        
+
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -22,7 +22,13 @@ urlpatterns = [
 ]
 """
 from django.urls import path, include
+from django.conf.urls import include
+
 
 urlpatterns = [
     path('', include('snippets.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
