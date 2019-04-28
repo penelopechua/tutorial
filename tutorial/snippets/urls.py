67e29a4bfe4f4dc9a7ca2a9e7214ed2dc36b77df
Penelope Chua <pche99@gmail.com>
2019-04-29 00:59:36 +0800

    Add endpoints for User models

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -20,6 +20,8 @@ from snippets import views
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns) - keep getting error for this, not sure why
